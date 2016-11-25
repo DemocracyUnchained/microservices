cec8dd9aff4cd41a673a4066378327bfa65cc961
Jon Radoff <jradoff@JonDev-5.home>
2016-11-25 12:06:54 -0500

    added license

--- rest.go
+++ rest.go
@@ -1,3 +1,14 @@
+
+/************************************************************************************************
+ * Copyright 2016 Democracy Unchained -- http://DemocracyUnchained.org
+ *
+ * Licensed under the GNU General Public License. See LICENSE.txt for details.
+ *
+ * Note: other non-source-code works from Democracy Unchained are typically licensed differently
+ * (typically Creative Commons with Attribution).  Please refer to the relevant licensing details
+ * in those projects.
+ *************************************************************************************************/
+
 package main 
 
 import (
